bbb9b016c446ec5d4b2db170ddf479b3c6cfdac7
parisgapp <55924977+parisgapp@users.noreply.github.com>
2021-07-29 22:04:57 -0500

    NightHack

---
+++ MiniProject2/MiniProject2/ThirdViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ThirdViewController.swift
+//  MiniProject2
+//
+//  Created by Paris Gapp on 7/29/21.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    @IBOutlet weak var Question3: UILabel!
+    
+    @IBOutlet weak var Answer3: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func SoccerA1(_ sender: UIButton) {
+    }
+    
+    @IBAction func BasketballA2(_ sender: UIButton) {
+    }
+    
+    @IBAction func FootballA3(_ sender: UIButton) {
+    }
+    
+   
+    @IBAction func BaseballA4(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MiniProject2/MiniProject2/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Paris Gapp on 7/29/21.
+//
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var Question1: UILabel!
+    
+    @IBOutlet weak var Answer1: UILabel!
+    
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func PinkA1(_ sender: UIButton) {
+    }
+    
+    
+
+    @IBAction func GreenA2(_ sender: UIButton) {
+    }
+    
+    @IBAction func BlueA3(_ sender: UIButton) {
+    }
+    
+    @IBAction func YellowA4(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+}
